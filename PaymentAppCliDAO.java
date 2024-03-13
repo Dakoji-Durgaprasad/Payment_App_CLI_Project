03f720eaf4f27024ec6018fbf7d668d1d19422ca
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-03-13 00:32:56 -0700

    Update PaymentAppCliDAO.java

--- PaymentAppCliDAO.java
+++ PaymentAppCliDAO.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -24,23 +25,49 @@ public class PaymentAppCliDAO {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
 
-			public static void storeUserBankAcctDetails(BankAccount ba) throws SQLException {
+	public static void printUserDetails() throws SQLException {
+		User u = new User();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Payments_App_CLI", "root",
+					"root");
+			Statement st = con.createStatement();
+			String query = "select * from User_info";
+			ResultSet rs = st.executeQuery(query);
+			while (rs.next()) {
+				System.out.println(
+						rs.getInt("Id") + " : " + rs.getString("First_Name") + " : " + rs.getString("Last_Name") + " : "
+								+ rs.getLong("Phone_Number") + " : " + rs.getString("Date_Of_Birth") + " : "
+								+ rs.getString("Address") + " : " + rs.getString("Password"));
+			}
+			con.close();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void storeUserBankAcctDetails(BankAccount ba) throws SQLException {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Payments_App_CLI", "root",
 					"root");
 			Statement st = con.createStatement();
 			String query = "insert into BankAccount_Details(Account_Number,Acct_IFSC,Bank_Name,Bank_Acct_Pin,Acct_Type,UserId) "
-					+ "values('"+ba.getBankAcctNumber()+"','"+ba.getBankAcctIFSC()+"','"+ba.getBankAcctBankName()+"','"+ba.getBankAcctType()+"','"+ba.getBankAcctPin()+"','"+ba.getUserId()+"')";
-			
+					+ "values('" + ba.getBankAcctNumber() + "','" + ba.getBankAcctIFSC() + "','"
+					+ ba.getBankAcctBankName() + "','" + ba.getBankAcctType() + "','" + ba.getBankAcctPin() + "','"
+					+ ba.getUserId() + "')";
+
 			int rs = st.executeUpdate(query);
 			System.out.println(rs + "row/s effected");
 
 			con.close();
-			
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
